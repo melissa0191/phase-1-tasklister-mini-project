6dfce88e30929d969db472b419dc1d0bc7f707ef
melissavelasquezgreene <melissa0191@outlook.es>
2023-11-16 14:15:38 -0500

    adds new results

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  //  your code here
+  const form = document.querySelector("#create-task-form");
+  const newList = document.querySelector("#tasks");
+  
+  const stop = document.addEventListener("submit",(event)=> {
+        event.preventDefault();
+        const newTask = document.querySelector ("#new-task-description");
+        const taskInputLine = newTask.value;
+  
+        const tasksList = document.createElement ("li");
+        tasksList.textContent = taskInputLine;
+  
+        newList.appendChild(tasksList);
+        newTask.value = "";
+  
+  });
+  })
